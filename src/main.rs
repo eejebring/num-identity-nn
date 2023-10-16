b39cddd82e69050a28ef0a66718de2c03383a171
eejebring <dev@ejebring.com>
2023-10-16 17:49:29 +0200

    mnist data

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use mnist::MnistBuilder;
+use std::usize;
+
 fn main() {
-    println!("Hello, world!");
+    let data = MnistBuilder::new().finalize();
+
+    let mut counts: [usize; 10] = [0; 10];
+    for label in data.trn_lbl {
+        counts[usize::from(label)] += 1;
+    }
+    for index in 0..counts.len() {
+        println!("{}: {}", index, counts[index])
+    }
 }
